c23dcf7d30c03cdb4e43d072e0de6da3fd046a30
AanaaAb1 <aanaaabdallaa9@gmail.com>
2026-08-11 02:42:46 +0300

    feat: add f string that put variable inside text

---
+++ 01-basics/f_strings.py
@@ -0,0 +1,18 @@
+# the programm is put the variable inside the text using f-string
+
+first_name = "Aana"
+last_name = "Abdella"
+
+fullname =f"Hello, Programmer! My name is {first_name} {last_name}"
+
+print(fullname)
+
+#the programm is to take the user input and put it inside the text using f-string
+
+user_first_name = input("Please enter your first name: ")
+
+user_last_name = input("Please enter your last name: ")
+
+user_fullname = f"Hello, Programmer! My name is {user_first_name} {user_last_name}"
+
+print(user_fullname)
---
+++ 01-basics/string_cases.py
@@ -0,0 +1,24 @@
+# the program below takes a string input from the user and converts it to uppercase
+
+user_input = input ("please enter the string you want to convert to uppercase:")
+
+uppercase_string = user_input.upper()
+
+print("The string in uppercase is:", uppercase_string)
+
+
+# Write the program that takes a string input from the user and converts it to lowercase.
+
+user_text = input(" Please enter the sring you want to convert to lowercase:")
+
+lowercase_string = user_text.lower();
+
+print("The string in lowercase is:", lowercase_string)
+
+# Write the program that takes a string input from the user and converts it to title case.
+
+user_string = input ("please enter the string you want to covert to title case:")
+
+convert_to_title_case = user_string.title()
+
+print("The string in title case is:", convert_to_title_case)
