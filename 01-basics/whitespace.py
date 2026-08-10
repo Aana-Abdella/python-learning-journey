2201764814eba0474a53e57a82c37b5277098b16
AanaaAb1 <aanaaabdallaa9@gmail.com>
2026-08-11 02:58:19 +0300

    feat: add the programm that remove the whitespace from leftside

--- 01-basics/whitespace.py
+++ 01-basics/whitespace.py
@@ -11,3 +11,33 @@ sripped_message = message.strip()
 #print the string after removing whitespace
 
 print("The string after removing whitespace is:", sripped_message)
+
+
+
+#the programm remove the whitespace from the string using lstrip() method
+
+original_message = "   Hello World   "
+
+print("The original string is:", original_message)
+
+# use the lstrip() method to remove whitespace from the left side of the string
+
+lstripped_message = original_message.lstrip()
+
+#print the string after removing whitespace from the left side
+
+print("The string after removing whitespace from the left side is:", lstripped_message)
+
+#the programm remove the whitespace from the string using rstrip() method
+
+unstripped_message = "   Hello World   "
+
+print("The original string is:", unstripped_message)
+
+# use the rstrip() method to remove whitespace from the right side of the string   
+
+rstripped_message = unstripped_message.rstrip()
+
+#print the string after removing whitespace from the right side
+
+print("The string after removing whitespace from the right side is:", rstripped_message)
