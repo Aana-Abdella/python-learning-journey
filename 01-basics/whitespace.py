911e5a6b07dcf90fbcedb2b7c67dd3be971f5584
AanaaAb1 <aanaaabdallaa9@gmail.com>
2026-08-11 02:51:17 +0300

    feat: add the programm that remove the whitspace from both sides of string

---
+++ 01-basics/whitespace.py
@@ -0,0 +1,13 @@
+# the programm remove the whitespace from the string using strip() method
+
+message = "   Hello World   "
+
+print("The original string is:", message)
+
+# use the strip() method to remove whitespace from the string
+
+sripped_message = message.strip()
+
+#print the string after removing whitespace
+
+print("The string after removing whitespace is:", sripped_message)
