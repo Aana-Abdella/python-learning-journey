e2d086564fd2ad93f879c471930f9054c71507a2
AanaaAb1 <aanaaabdallaa9@gmail.com>
2026-08-12 01:15:44 +0300

    feat: add the variable with multiple float  assignment and add them and print

--- 01-basics/variables.py
+++ 01-basics/variables.py
@@ -11,3 +11,9 @@ print(age)
 x, y, z = 10, 20, 30
 
 print("The summation of x, y, and z is =", x+y+z)  # Output: 60
+
+# Create a variable with a float value and print it.
+
+x, y, z = 10.5, 20.5, 30.5
+
+print("The summation of x, y, and z is =", x+y+z)  # Output: 61.5
