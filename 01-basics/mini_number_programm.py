e0eeb15a6a4b02672466aa936406d5b36a15b034
AanaaAb1 <aanaaabdallaa9@gmail.com>
2026-08-12 01:56:07 +0300

    feat: add mini number programm

---
+++ 01-basics/mini_number_programm.py
@@ -0,0 +1,18 @@
+# Build a small program that represents the score of a game.
+
+x, y, z = 10, 20, 30
+
+total_score = x + y + z
+
+# the difference between the scores of the players
+
+score_difference = max(x, y, z) - min(x, y, z)
+
+# product of the scores of the players
+score_product = x * y * z
+
+# Division involving the scores of the players
+score_division = x / y / z
+
+# An exponentiation calculation involving the scores of the players
+score_exponentiation = x ** y ** z
