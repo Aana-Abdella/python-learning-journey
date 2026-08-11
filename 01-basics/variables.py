5f4667c462c162222473db7d87f1bb7afa1f7172
AanaaAb1 <aanaaabdallaa9@gmail.com>
2026-08-12 01:14:18 +0300

    feat: add the variable with multiple assignment

--- 01-basics/variables.py
+++ 01-basics/variables.py
@@ -4,3 +4,10 @@ name = "Alice"
 age = 25
 print(name)
 print(age)
+
+
+# Create three variables using multiple assignment.
+
+x, y, z = 10, 20, 30
+
+print("The summation of x, y, and z is =", x+y+z)  # Output: 60
