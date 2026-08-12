2e4f76ce3d1ff4f6772db0c85840af2c825f87d1
AanaaAb1 <aanaaabdallaa9@gmail.com>
2026-08-13 02:32:25 +0300

    feat: add programm that convert the list to the string

---
+++ 01-basics/04_lists/convert_to_string.py
@@ -0,0 +1,7 @@
+# Converting a list to a string and vice versa
+
+is_list = ["Python", "is", "awesome", "and", "I", "love", "it"]
+
+python_string = ", ".join(is_list)  # Converting a list to a string
+
+print(python_string)  # Output: Python is awesome and I love it
