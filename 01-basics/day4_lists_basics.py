576102b54372e4951b1b9bae9527894597a1e497
AanaaAb1 <aanaaabdallaa9@gmail.com>
2026-08-13 00:41:28 +0300

    feat: length of lists

--- 01-basics/day4_lists_basics.py
+++ 01-basics/day4_lists_basics.py
@@ -2,4 +2,8 @@
 
 thislist = [1, 2, 3, 4, 5]
 
-print(thislist)  # Output: [1, 2, 3, 4, 5]
+print(thislist)  # Output: [1, 2, 3, 4, 5] 
+
+# The length of the list can be found using the len() function
+
+print(len(thislist)) 
