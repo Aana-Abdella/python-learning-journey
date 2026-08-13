7c225df05de83b48cc947ea74387215fa84cac6b
AanaaAb1 <aanaaabdallaa9@gmail.com>
2026-08-14 00:06:24 +0300

    feat: add the multiple of items in the list

---
+++ 01-basics/04_lists/multiple_item.py
@@ -0,0 +1,22 @@
+
+# The Programm that multipy the items in the list
+
+def multiple_item (items):
+
+
+    tot = 1
+
+
+    for x in items:
+
+     tot *= x
+
+
+    return tot
+
+
+items = [ 23, 3, 5 ]
+
+
+print(multiple_item([ 23, 3, 5 ]))
+
