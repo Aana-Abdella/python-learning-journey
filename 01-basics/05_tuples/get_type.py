044ea4cb121809363cee3fdf2f42b6dc9d95956a
AanaaAb1 <aanaaabdallaa9@gmail.com>
2026-08-14 01:17:42 +0300

    feat: get the type of tuple using map() method

--- 01-basics/05_tuples/get_type.py
+++ 01-basics/05_tuples/get_type.py
@@ -13,3 +13,10 @@ this_tuple = ("apple", "banana", "cherry", 10,  True, 3.14, None)
 types = [type(item) for item in this_tuple]
 
 print(types)  # Output: [<class 'str'>, <class 'str'>, <class 'str'>, <class 'int'>, <class 'bool'>, <class 'float'>, <class 'NoneType'>]
+
+# get the type of tuple using the map() method
+
+tuple_types = list(map(type, this_tuple))
+
+print(tuple_types) 
+
