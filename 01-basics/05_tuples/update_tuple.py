a8fd4f07ff980e6f2bdf54ddef7bb8dfd63f52c4
AanaaAb1 <aanaaabdallaa9@gmail.com>
2026-08-17 02:10:41 +0300

    feat: add join two tuples using add them and multiply by 2 to duplicate the items of tuple

--- 01-basics/05_tuples/update_tuple.py
+++ 01-basics/05_tuples/update_tuple.py
@@ -14,12 +14,20 @@ x = ("apple", "banana", "cherry")
 
 y = list(x)
 
+# Appenf
+
 y.append("orange")
 
 x = tuple(y)
 
 print(x)
 
+#Remove items
+
+y.remove("banana")
+x = tuple(y)
+
+print(x)
 
 # Add tuple to tuple
 
@@ -30,3 +38,21 @@ add = ("mango","apple")
 fruit_tuple += add
 
 print(fruit_tuple)
+
+
+#Join two tuples by using add them
+
+tuple1 = ("Pyramid", "Stone")
+tuple2 = ("oromo", "Ethiopia")
+
+tuple3 = tuple1 + tuple2
+
+print(tuple3)
+
+# Multiple Tuples
+
+tuple4 = ("Hararii", "Dire Dawa")
+
+tuple5 = tuple4 *2
+
+print(tuple5)
