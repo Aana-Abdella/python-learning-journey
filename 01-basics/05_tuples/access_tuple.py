3df2ee5859d113ec8ae77a5b0aa64fdcc51ec339
AanaaAb1 <aanaaabdallaa9@gmail.com>
2026-08-17 00:43:52 +0300

    feat: add the programm access tuple using -ve indexing

--- 01-basics/05_tuples/access_tuple.py
+++ 01-basics/05_tuples/access_tuple.py
@@ -3,3 +3,7 @@
 my_tuple = ("apple", "banana", "cherry", 10,  True, 3.14, None)
 
 print(my_tuple[0])  # Output: apple
+
+# Negative indexing to access the last element of the tuple
+
+print(my_tuple[-1])  # Output: None
