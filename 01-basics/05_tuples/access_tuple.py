10eb226de9c06ec467087c7de77badb1c41b58c6
AanaaAb1 <aanaaabdallaa9@gmail.com>
2026-08-17 01:12:29 +0300

    feat: add programm to access tuple using check and -ve slicing step

--- 01-basics/05_tuples/access_tuple.py
+++ 01-basics/05_tuples/access_tuple.py
@@ -20,6 +20,11 @@ print(my_tuple[-4:-1])  # Output: (10, True, 3.14)
 
 print(my_tuple[::2])  # Output: ('apple', 'cherry', True, None)
 
+# accessing elements using a negative step value in slicing
+
+print(my_tuple[::-1])  # Output: (None, 3.14, True, 10, 'cherry', 'banana', 'apple')
+
+print(my_tuple[1:5:2])  # Output: ('banana', 10)
 # Checking if an element exists in the tuple using the 'in' keyword
 
 if "apple" in my_tuple:
