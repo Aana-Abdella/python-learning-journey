0190f4e8b28e9e47b2583160484905ab8a0f3da4
AanaaAb1 <aanaaabdallaa9@gmail.com>
2026-08-17 04:06:00 +0300

    feat:add another method slot-machine projet

--- projects/Slot-machine/main.py
+++ projects/Slot-machine/main.py
@@ -4,7 +4,7 @@ MIN_BET = 10
 
 def deposit():
     while True:
-        amount = input("What ever you want $")
+        amount = input("How much would you like to deposit$")
         if amount.isdigit():
            amount = int(amount)
            if amount > 0:
@@ -28,12 +28,33 @@ def get_number_of_lines():
         else:
             print("Please Enter a number")
     return lines
-
+def get_bet():
+    while True:
+            amount = input("What would you like to bet on each line? $")
+            if amount.isdigit():
+               amount = int(amount)
+               if MIN_BET <=amount <= MAX_BET:
+                   break
+               else:
+                   print(f"Amount is must be between ${MIN_BET} - ${MAX_BET}.")
+            else:
+                print("Please Enter a number")
+    return amount
     
 
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    print(balance, lines)
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet >= balance:
+            print(f"you dont have enough balance to bet that amount, your current balance is: ${balance}")
+
+        else:
+            break
+
+    print(f"you are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
 
 main()
