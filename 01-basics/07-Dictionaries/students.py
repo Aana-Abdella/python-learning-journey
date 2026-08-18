105d11910d62626da286f126149960c653552152
AanaaAb1 <aanaaabdallaa9@gmail.com>
2026-08-19 02:03:17 +0300

    feat:checking in items in students data  dictionary file

--- 01-basics/07-Dictionaries/students.py
+++ 01-basics/07-Dictionaries/students.py
@@ -12,8 +12,13 @@ print("before anything applied")
 print(students)
 print("the lenths of students is:" ,len(students))
 
+# Add or modify
+
 print("Adding or modifying items to dictionarries")
 students["skills"] = ['Python','Scripting','javascript','node','AI-Autimation']
+print(students["skills"][0])
+print(students["skills"])
+
 
 # Accessing students using key or indexing anything else.
 
@@ -27,3 +32,11 @@ print("After method or constructor or funtction applied")
 print(students.pop("f_name"))
 print(students.popitem())
 del students["city"]
+
+
+# Checking in students
+
+print("after checking in")
+
+print("address" in students)
+print("age" in students)
