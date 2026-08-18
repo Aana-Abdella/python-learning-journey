f00f7a8540414739a7e686b6da0921882ab1bbc8
AanaaAb1 <aanaaabdallaa9@gmail.com>
2026-08-19 02:24:55 +0300

    feat:deleting, get method,get keys  items in students data  dictionary file

--- 01-basics/07-Dictionaries/students.py
+++ 01-basics/07-Dictionaries/students.py
@@ -40,3 +40,40 @@ print("after checking in")
 
 print("address" in students)
 print("age" in students)
+
+# Copy from students
+
+print("The new and copied from the fist dictionary")
+
+new_dict = students.copy()
+
+new_dict["f_name"] = "Aanaa"
+
+print(new_dict)
+
+# clear students after copy to new_dict 
+print(" clear students after copy to new_dict ")
+students.clear()
+print(students)
+
+#get keys of dictionaries
+
+keys = students.keys()
+new_keys = new_dict.keys()
+
+values = new_dict.values()
+print(keys)
+print(new_keys)
+print(values)
+print(new_dict.get("f_name"))
+
+third_dict = new_dict.copy()
+
+
+#delete dict
+print("after delete nothing is here")
+del new_dict
+
+print("but there is the third dict it copied from new_dict before delete")
+
+print(third_dict)
