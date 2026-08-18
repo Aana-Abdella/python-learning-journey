0f8575488e9cfe937471f6ad546ca5b676dd8c3a
AanaaAb1 <aanaaabdallaa9@gmail.com>
2026-08-18 20:44:14 +0300

    feat:add access set programm

---
+++ 01-basics/06-sets/access_sets.py
@@ -0,0 +1,12 @@
+thisset = {"apple","banana","cherry"}
+print(thisset)
+
+# Use for loop To print each items by extracting from curly bracket
+
+for x in thisset:
+    print(x)
+
+# to print data type
+print(type(thisset))
+
+print("banana" in thisset)
