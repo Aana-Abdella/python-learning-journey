f09051e793124ddd8eb3f1ba88f5b6e5d3611fd2
AanaaAb1 <aanaaabdallaa9@gmail.com>
2026-08-19 01:41:02 +0300

    feat:add students data  dictionary file

---
+++ 01-basics/07-Dictionaries/students.py
@@ -0,0 +1,15 @@
+students = {
+    "f_name" : "Aanaa",
+    "l_name" : "Abdella",
+    "gender" : "male",
+    "status" : "single",
+    "countey" : "Ethipoia",
+    "city" : "Karamile",
+    "address" : "Dire-Dawa"
+}
+print(students)
+print(len(students))
+
+# Change to list
+
+print(students.items())
