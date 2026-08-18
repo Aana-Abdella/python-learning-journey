39b00644ec3f7c5eec5af49c2688349b31381f30
AanaaAb1 <aanaaabdallaa9@gmail.com>
2026-08-18 21:28:57 +0300

    feat:add the different way of removes item from set

---
+++ 01-basics/06-sets/remove_item.py
@@ -0,0 +1,25 @@
+my_sets = {"Aanaa","Chala", "Obssaa","Fahmi"}
+
+my_sets.remove("Aanaa")
+
+print(my_sets)
+
+my_sets.discard("Fahmi")
+
+print(my_sets)
+
+# Using pop
+
+thisset = {"ana","game","baadhee","mage"}
+
+x = thisset.pop()
+
+print(x)
+print(thisset)
+
+# Use del 
+
+the_set = {"feog","fish","corocodile"}
+
+del the_set
+print(the_set) # it produce error b/c the_set completely removed
