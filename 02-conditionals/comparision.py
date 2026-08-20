7926bd9df7faefd3f27993139e01d22974d252f7
AanaaAb1 <aanaaabdallaa9@gmail.com>
2026-08-21 01:03:01 +0300

    feat: add checking student younger or adult

---
+++ 02-conditionals/comparision.py
@@ -0,0 +1,11 @@
+age = int(input("Please enter your age ?"))
+
+student = int(input("Please enter 1 if you are student or 0 if not student"))
+
+if age >= 18 and student == 1 :
+
+    print("You are adult student")
+else:
+    print("You are younger student")
+
+                            
