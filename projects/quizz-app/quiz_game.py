3a4391bb8fc8dec07837364f762d348969ca3b33
AanaaAb1 <aanaaabdallaa9@gmail.com>
2026-09-02 17:24:50 +0300

    feat: initial commit of quizz app in python

---
+++ projects/quizz-app/quiz_game.py
@@ -0,0 +1,13 @@
+print("Welcome to The guizz game")
+
+playing = input("Do you want to play? ")
+
+if playing != "yes":
+    quit()
+
+print('Let play')
+
+answer = input('what is CPU stands for ? ')
+
+if answer == 'central processing unit' :
+    print('correct')
