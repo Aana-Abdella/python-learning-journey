37c0815e428a8f280d62525c64e16aa629b126e4
AanaaAb1 <174483431+Aana-Abdella@users.noreply.github.com.>
2026-09-07 04:10:30 +0300

    feat : factorial and add error handling

--- 04-Functions/exercises/factorial.py
+++ 04-Functions/exercises/factorial.py
@@ -1,9 +1,19 @@
 def factorial(n):
 
-    if n == 0 : 
+    if n < 0:
+        raise ValueError("Factorial is not defined for negative numbers.")
+    elif n== 0 or n == 1:
         return 1
-    else:            
-        return n * factorial(n - 1)
+    else:
+        result = 1
+        for i in range(2, n + 1):
+            result *= i
+        return result
+
+    try:
+        print(f"The factorial of {n} is {factorial(n)}")
+    except ValueError as e:
+        print(e)    
 
 n = int(input("Enter a number to calculate its factorial: "))
 
