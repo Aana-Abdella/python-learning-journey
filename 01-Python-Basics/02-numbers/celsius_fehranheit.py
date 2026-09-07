7cfee788433c02b8d96f657af08bf008a71b62d9
Aana Abdella <174483431+Aana-Abdella@users.noreply.github.com>
2026-09-07 23:53:52 +0300

    Create celsius_fehranheit.py
    change celsius temperature to fahrenheit temperature

---
+++ 01-Python-Basics/02-numbers/celsius_fehranheit.py
@@ -0,0 +1,9 @@
+celsius_temp  =  90
+fahrenheit = (celsius_temp  *  (9/5) )  +  32
+print(fahrenheit)
+
+#or
+
+cel_temp  = float(input("Enter Celsius Temperature: "))
+fahrenheit = (cel_temp  *  (9/5) )  +  32
+print(fahrenheit)
