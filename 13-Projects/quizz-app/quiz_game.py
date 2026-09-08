aeb54e42c687fb948c685ae7bd516fce0fd81ac9
AanaaAb1 <174483431+Aana-Abdella@users.noreply.github.com>
2026-09-09 00:50:51 +0300

    feat: mini quiz app python proct

--- 13-Projects/quizz-app/quiz_game.py
+++ 13-Projects/quizz-app/quiz_game.py
@@ -2,12 +2,48 @@ print("Welcome to The guizz game")
 
 playing = input("Do you want to play? ")
 
-if playing != "yes":
+if playing.lower() != "yes":
     quit()
 
-print('Let play')
+print('Let lets start quizz')
 
 answer = input('what is CPU stands for ? ')
-
-if answer == 'central processing unit' :
+score = 0 
+if answer.lower() == 'central processing unit' :
     print('correct')
+    score += 1
+
+else:
+    print('incorrect')
+
+answer = input('what is GPU stands for ? ')
+
+if answer.lower() == 'graphical processing unit' :
+    print('correct')
+    score += 1
+
+else:
+    print('incorrect')
+
+answer = input("What RAM stands for ? ")
+
+if answer.lower() == 'random access memory' :
+    print("Correct!")
+    score += 1
+
+else :
+    print("Incorrect")
+
+answer = input("What USB stands for ? ")
+
+if answer.lower() == "universal serial bus" :
+    print("Correct!")
+    score += 1
+else :
+    print("Incorrect")
+
+
+print(f"you scored {score} . ")
+print("you got " + str(score) + " questions correct!")
+print("you got " + str(4 - score) + " questions incorrect!")
+print("you got " + str((score / 4) * 100) + "%")
