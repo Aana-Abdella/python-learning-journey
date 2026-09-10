2bb63ac3ddefab6ec30ad844f3a7df4965d0bcf7
AanaaAb1 <174483431+Aana-Abdella@users.noreply.github.com>
2026-09-10 03:11:17 +0300

    implement transfer functionality

--- 08-OOP/bank-system/main.py
+++ 08-OOP/bank-system/main.py
@@ -7,16 +7,23 @@ class BankAccount:
     def deposit(self, amount):
         if amount > 0:
             self.balance += amount
-            print(f"Deposited {amount}. New balance is {self.balance}.")
+            print(f"Dear {self.account_holder}  {amount} Deposited to your Account and your New balance is {self.balance}.")
         else:
             print("Deposit amount must be positive.")
 
     def withdraw(self, amount):
         if 0 < amount <= self.balance:
             self.balance -= amount
-            print(f"Withdrew {amount}. New balance is {self.balance}.")
+            print(f"Dear {self.account_holder}, you have Withdrew {amount}. New balance is {self.balance}.")
         else:
             print("Invalid withdrawal amount.")
+    def transfer(self, amount, recipient_account):
+        if 0 < amount <= self.balance:
+            self.balance -= amount
+            recipient_account.deposit(amount)
+            print(f"Dear {self.account_holder}, you have Transferred {amount} to {recipient_account.account_holder}. New balance is {self.balance}.")
+        else:
+            print("Invalid transfer amount.")
 
     def get_balance(self):
         return self.balance
@@ -24,7 +31,9 @@ class BankAccount:
     def __str__(self):
         return f"Account Number: {self.account_number}, Account Holder: {self.account_holder}, Balance: {self.balance}"
 
-account1 = BankAccount("123456", "Alice", 1000)
+    
+
+account1 = BankAccount("123456", "Alice", 0)
 account1.deposit(500)
 account1.withdraw(200)
 print(account1)
@@ -34,7 +43,11 @@ account2.deposit(300)
 account2.withdraw(100)
 print(account2)
 
-account3 = BankAccount("345678", "Charlie", 2000)
+account3 = BankAccount("345678", "Charlie", 0)
 account3.deposit(1000)
 account3.withdraw(500)
 print(account3)
+
+account1.transfer(300, account2)
+print(account1)
+print(account2)
