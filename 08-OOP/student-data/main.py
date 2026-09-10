6e81a6693a7a7665be33d8ca5c52aa098b0207ce
AanaaAb1 <174483431+Aana-Abdella@users.noreply.github.com>
2026-09-10 03:54:51 +0300

    student average, introduce, study using oop

---
+++ 08-OOP/student-data/main.py
@@ -0,0 +1,24 @@
+class Student:
+    def __init__(self, name, age, grade, mark ):
+        self.name = name
+        self.age = age
+        self.grade = grade
+        self.mark = mark
+
+    def get_introduce(self):
+        return f"My name is {self.name}, I am {self.age} years old, and This is my {self.grade}th years in campus ."
+    def study(self, subject):
+        return f"{self.name} is studying {subject}."
+    def calculate_average(self):
+        total = 0
+        
+        for a in self.mark :
+            total += a
+        return total / len(self.mark) 
+
+student1 = Student("Aanaa", 23, 4, [50,60,70 ])
+print(student1.get_introduce())
+print(student1.study("Software Engineering"))
+print(student1.calculate_average())
+
+
